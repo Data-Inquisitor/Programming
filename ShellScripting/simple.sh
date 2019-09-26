146fb4e7584ec2a3abd5d5098d6923c6011753c6
Vivek Nagaraj <vnaga08@gmail.com>
2019-09-26 07:48:36 -0500

    Added some additional lines to simple.sh. It doesn't work at this point. Incorrectly using the command -sed.

--- ShellScripting/simple.sh
+++ ShellScripting/simple.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "First argument is $1 and the Second argument is $2"
+HOST="$(hostname)"
+MAC="$(ifconfig wifi0 | grep ether)"
+echo "The MAC address for ${HOST} wifi is $(sed -e 's/ether/""/' ${MAC})"
